2a33a0b70da86055281ab2ea6fd2e14e90be9d51
John Quinn <johnedquinn@gmail.com>
2025-03-04 21:13:12 -0800

    Adds console log and removes tags on folder page

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -60,18 +60,6 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
                   </a>
                 </h3>
               </div>
-              <ul class="tags">
-                {tags.map((tag) => (
-                  <li>
-                    <a
-                      class="internal tag-link"
-                      href={resolveRelative(fileData.slug!, `tags/${tag}` as FullSlug)}
-                    >
-                      {tag}
-                    </a>
-                  </li>
-                ))}
-              </ul>
             </div>
           </li>
         )
